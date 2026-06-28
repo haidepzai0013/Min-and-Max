385a25ab4c8013675e05d9637fd8491f232f56b9
Hải Nguyễn <haidepzai0013@gmail.com>
2026-06-28 06:23:53 +0000

    add english code

--- code-english/code-eng.c
+++ code-english/code-eng.c
@@ -17,4 +17,25 @@ int main() {
         scanf("%d", &b);
     }
     int matrix[a][b];
+    for(m = 0; m< a; m++) {
+        printf("Enter the value in row %d: \n", m);
+        for(n = 0; n < b; n++) {
+            printf("[%d][%d]: ", m, n);
+            scanf("%d", &matrix[m][n]);
+        }
+    }
+    int max = matrix[0][0];
+    int min = matrix[0][0];
+    for(m = 0; m < a; m++) {
+        for(n = 0; n< b; n++) {
+            if(matrix[m][n] > max) {
+                max = matrix[m][n];
+            }
+            if(matrix[m][n] < min) {
+                min = matrix[m][n];
+            }
+        }
+    }
+    printf("The maximum value of the matrix is: %d\nThe minimum value of your matrix is: %d\n", max, min);
+    return 0;
 }
